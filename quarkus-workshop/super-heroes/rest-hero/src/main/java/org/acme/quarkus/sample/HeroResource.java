132f5ca72249d76bf73104d149bb9a3b9cbbe42b
Nagabhushanam <nagabhushanamn@gmail.com>
2020-09-23 16:46:20 +0530

    super heroes : microservices

--- quarkus-workshop/super-heroes/rest-hero/src/main/java/org/acme/quarkus/sample/HeroResource.java
+++ quarkus-workshop/super-heroes/rest-hero/src/main/java/org/acme/quarkus/sample/HeroResource.java
@@ -5,7 +5,6 @@ package org.acme.quarkus.sample;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -23,6 +22,9 @@ import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 //import org.eclipse.microprofile.openapi.annotations.Operation;
 //import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 //import org.eclipse.microprofile.openapi.annotations.media.Content;
@@ -41,6 +43,10 @@ public class HeroResource {
 
 //    @Operation(summary = "Returns a random hero")
 //    @APIResponse(responseCode = "200", content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = Hero.class, required = true)))
+    // tag::adocMetrics[]
+    @Counted(name = "countGetRandomHero", description = "Counts how many times the getRandomHero method has been invoked")
+    @Timed(name = "timeGetRandomHero", description = "Times how long it takes to invoke the getRandomHero method", unit = MetricUnits.MILLISECONDS)
+    // end::adocMetrics[]
     @GET
     @Path("/random")
     public Response getRandomHero() {
@@ -52,6 +58,10 @@ public class HeroResource {
 //    @Operation(summary = "Returns all the heroes from the database")
 //    @APIResponse(responseCode = "200", content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = Hero.class, type = SchemaType.ARRAY)))
 //    @APIResponse(responseCode = "204", description = "No heroes")
+ // tag::adocMetrics[]
+    @Counted(name = "countGetAllHeroes", description = "Counts how many times the getAllHeroes method has been invoked")
+    @Timed(name = "timeGetAllHeroes", description = "Times how long it takes to invoke the getAllHeroes method", unit = MetricUnits.MILLISECONDS)
+    // end::adocMetrics[]
     @GET
     public Response getAllHeroes() {
         List<Hero> heroes = service.findAllHeroes();
@@ -62,6 +72,10 @@ public class HeroResource {
 //    @Operation(summary = "Returns a hero for a given identifier")
 //    @APIResponse(responseCode = "200", content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = Hero.class)))
 //    @APIResponse(responseCode = "404", description = "The hero is not found for a given identifier")
+    // tag::adocMetrics[]
+    @Counted(name = "countGetHero", description = "Counts how many times the getHero method has been invoked")
+    @Timed(name = "timeGetHero", description = "Times how long it takes to invoke the getHero method", unit = MetricUnits.MILLISECONDS)
+    // end::adocMetrics[]
     @GET
     @Path("/{id}")
     public Response getHero(@PathParam("id") Long id) {
@@ -78,6 +92,10 @@ public class HeroResource {
 //    @Operation(summary = "Creates a valid hero")
 //    @APIResponse(responseCode = "201", description = "The URI of the created hero", content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = URI.class)))
 //    @APIResponse(responseCode = "400", description = "Invalid input")
+    // tag::adocMetrics[]
+    @Counted(name = "countCreateHero", description = "Counts how many times the createHero method has been invoked")
+    @Timed(name = "timeCreateHero", description = "Times how long it takes to invoke the createHero method", unit = MetricUnits.MILLISECONDS)
+    // end::adocMetrics[]
     @POST
     public Response createHero(@Valid Hero hero, @Context UriInfo uriInfo) {
         hero = service.persistHero(hero);
@@ -88,6 +106,10 @@ public class HeroResource {
 
 //    @Operation(summary = "Updates an exiting  hero")
 //    @APIResponse(responseCode = "200", description = "The updated hero", content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = Hero.class)))
+    // tag::adocMetrics[]
+    @Counted(name = "countUpdateHero", description = "Counts how many times the updateHero method has been invoked")
+    @Timed(name = "timeUpdateHero", description = "Times how long it takes to invoke the updateHero method", unit = MetricUnits.MILLISECONDS)
+    // end::adocMetrics[]
     @PUT
     public Response updateHero(@Valid Hero hero) {
         hero = service.updateHero(hero);
@@ -97,6 +119,10 @@ public class HeroResource {
 
 //    @Operation(summary = "Deletes an exiting hero")
 //    @APIResponse(responseCode = "204")
+ // tag::adocMetrics[]
+    @Counted(name = "countDeleteHero", description = "Counts how many times the deleteHero method has been invoked")
+    @Timed(name = "timeDeleteHero", description = "Times how long it takes to invoke the deleteHero method", unit = MetricUnits.MILLISECONDS)
+    // end::adocMetrics[]
     @DELETE
     @Path("/{id}")
     public Response deleteHero(@PathParam("id") Long id) {
